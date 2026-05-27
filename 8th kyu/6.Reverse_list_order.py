a671afa66e6576e66272df98f235e7a1c06d52b1
David Uguet <daviduguet@192.168.1.94>
2026-05-27 15:54:08 +0200

    27/05/2026 New Kata

---
+++ 8th kyu/6.Reverse_list_order.py
@@ -0,0 +1,12 @@
+
+"""
+In this kata you will create a function that takes in a list and returns a list with the reverse order.
+"""
+
+def reverse_list(l):
+    l_reverse = l[::-1]
+    return l_reverse
+
+lista = [1, 2, 3, 4]
+
+print(reverse_list(lista))
